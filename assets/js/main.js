860a38cf1edb62fe74e32bcefc6fe06e95c0f645
n3ytan <109997116+N3ytan@users.noreply.github.com>
2022-09-08 15:14:38 -0300

    starting jobs with javascript

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+
+const txtName = document.getElementById('txtName')
+const txtEmail = document.getElementById('txtEmail')
+const txtLevel = document.getElementById('txtLevel')
+const txtCharacter = document.getElementById('txtCharacter')
+
+const btnSubscribe = document.getElementById('btnSubscribe')
+
+btnSubscribe.addEventListener('click', () => {
+    console.log ( "Passou por aqui!" )
+})
